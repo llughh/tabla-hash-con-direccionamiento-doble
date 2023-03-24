1e8dc4074446a519660c6db5ada85591f73a614e
llughh <llughprogramacion@gmail.com>
2023-03-24 19:53:27 +0100

    creation classes

---
+++ Celda.java
@@ -0,0 +1,11 @@
+//
+// class Celda
+// author: Vicente David Mut and Juan Carlos Sanchez
+// date: 24/03/2023
+//
+public class Celda {
+//    H(clave, colisiones) = H1 (clave) + H2 (clave, colisiones)
+//    H1 (clave) = clave mod N
+//    H2 (clave, colisiones) = colisiones * (7 – (clave mod 7))
+
+}
