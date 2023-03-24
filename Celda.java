04e69276f38872f09c4e76e1b5c501018e4c711e
llughh <llughprogramacion@gmail.com>
2023-03-24 20:09:02 +0100

    create all functions

--- Celda.java
+++ Celda.java
@@ -7,5 +7,27 @@ public class Celda {
 //    H(clave, colisiones) = H1 (clave) + H2 (clave, colisiones)
 //    H1 (clave) = clave mod N
 //    H2 (clave, colisiones) = colisiones * (7 – (clave mod 7))
+    public static void main(String[] args) {
+    }
+    public boolean setEstado(int valorEstado){
+        return true;
+    }
+    public void setClave(int valorClave){
 
+    }
+    public void setValor(){
+
+    }
+    public int getEstado(){
+        return 0;
+    }
+    public int getClave(){
+        return 0;
+    }
+    public void getValor(){
+
+    }
+    public boolean equals(){
+        return true;
+    }
 }
