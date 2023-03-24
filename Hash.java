8a52eb4be4d04c27a240ac73bd1883d9a634a9d4
llughh <llughprogramacion@gmail.com>
2023-03-24 20:24:24 +0100

    created all functions for hash class

--- Hash.java
+++ Hash.java
@@ -4,4 +4,54 @@
 // date: 24/03/2023
 //
 public class Hash {
+    public static void main(String[] args) {
+    }
+    public void insertar(int numeroInsertar){
+
+    }
+    public boolean borrar(int claveBorrar){
+        return true;
+    }
+    public void get(){
+
+    }
+    public boolean esVacia(){
+        return true;
+    }
+    public float getAlfaMax(){
+        return 0;
+    }
+    public void setAlfaMax(){
+
+    }
+    public int getNumElementos(){
+        return 0;
+    }
+    public float factorCarga(){
+        return 0;
+    }
+    private boolean hayColision(){
+        return true;
+    }
+    private int funcionHash(){
+        return 0;
+    }
+    private int hash(){
+        return 0;
+    }
+    private int hash2(){
+        return 0;
+    }
+    private void redimensionar(){
+
+    }
+    private boolean esPrimo(){
+        return true;
+    }
+    private int siguientePrimo(){
+        return 0;
+    }
+    public String toString(){
+        return "";
+    }
 }
