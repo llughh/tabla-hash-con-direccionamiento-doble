1eae8f236c878732dbb4be69164a2c9127c30bdb
Juan Carlos Sanchez <77439369+Juanki019@users.noreply.github.com>
2023-04-17 16:46:42 +0200

    v5 - last version

--- Celda.java
+++ Celda.java
@@ -8,47 +8,93 @@
  - Version: 1
 */
 
-public class Celda<Valor> {
 
+/**
+ * Clase generica que contine una clave, valor y un estado.
+ * @param <Valor> Tipo de valor contenido en la celda.
+ */
+public class Celda<Valor> {
     private int clave;
     Valor valor;
     private int estado;
 
+    /**
+     * Contructor sin parametros.
+     */
     public Celda(){
         this.estado = 0;
         this.clave = 0;
         this.valor = null;
     }
+
+    /**
+     * Constructor que toma una clave y un valor como parametros.
+     * @param clave La clave de la celda.
+     * @param v El valor de la celda.
+     */
     public Celda(int clave, Valor v){
         this.clave = clave;
         this.valor = v;
     }
+
+    /**
+     * Establece el Estado de la celda
+     * @param estado Estado de la celda que puede ser unicamente 1, 0, -1.
+     * @return true si puedo establcer el Estado / false si no.
+     */
     public boolean setEstado(int estado){
         if(estado == 1 || estado == -1 || estado == 0) {
             this.estado = estado;
+            return true;
         }
         return false;
     }
+
+    /**
+     * Establece la Clave de la Celda.
+     * @param clave
+     */
     public void setClave(int clave){
         this.clave = clave;
     }
 
+    /**
+     * Establece el valor de la Celda.
+     * @param v
+     */
     public void setValor(Valor v){
         this.valor = v;
     }
 
+    /**
+     * Obtiene el Estado de la Celda
+     * @return
+     */
     public int getEstado(){
         return estado;
     }
 
+    /**
+     * Obtiene la Clave de la Celda
+     * @return
+     */
     public int getClave(){
         return clave;
     }
 
+    /**
+     * Obtiene el Valor de la Celda
+     * @return
+     */
     public Valor getValor(){
         return valor;
     }
 
+    /**
+     * Obtiene si dos Celdas son iguales
+     * @param object Objeto a compara con la Celda
+     * @return ture si las Celdas son iguales / false si no lo son
+     */
     public boolean equals(Object object){
 
         if (object instanceof Celda){
@@ -58,4 +104,3 @@ public class Celda<Valor> {
         return false;
     }
 }
-
